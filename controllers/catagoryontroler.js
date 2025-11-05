57820c124d51bae8d5391b3fd85dbf43ff2a9295
Amanuel Shibabaw <amanuelshibabaw915@gmail.com>
2025-11-05 13:43:56 +0300

    catagory controller exported

---
+++ controllers/catagoryontroler.js
@@ -0,0 +1,12 @@
+const Catagory = require('../models/Catagory')
+
+const addCatag = async(req,resizeBy,next)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+module.exports = {addCatag}
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const authController = require('./auth')
-
+const catagoryController = require('./catagoryontroler')
 module.exports = {
-    authController
+    authController,
+    catagoryController
 }
